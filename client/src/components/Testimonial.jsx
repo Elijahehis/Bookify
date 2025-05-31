ae3f00f747a092eb8af6202152adf8fb3c7940fa
elijah <elijahehis@gmail.com>
2025-05-31 21:55:33 +0100

    Fix

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -6,7 +6,7 @@ import StarRating from './StarRating'
 const Testimonial = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 pt-20 pb-30'>
-      <Title title="What Our Guests Say" subtitle="Discover why discerning travelers choose QuickStay for their luxury accommodations around the world." />
+      <Title title="What Our Guests Say" subtitle="Discover why discerning travelers choose Bookify for their luxury accommodations around the world." />
       <div className="flex flex-wrap items-center gap-6 mt-20">
         {testimonials.map((testimonial) => (
           <div key={testimonial.id} className="bg-white p-6 rounded-xl shadow">
