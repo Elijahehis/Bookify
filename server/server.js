5dd09eff9212d858245403a60dbbad1ca7a96ff9
codetitan <codetitan2206@gmail.com>
2025-06-25 19:18:56 +0100

    added cors

--- server/server.js
+++ server/server.js
@@ -12,42 +12,50 @@ import bookingRouterRouter from "./routes/bookingRoutes.js";
 import { stripeWebHooks } from "./controller/stripeWebhooks.js";
 
 const startServer = async () => {
-    try {
-        await connectDB(); // 🧠 Wait for MongoDB to connect before starting the server
-        connectCloudinary()
+  try {
+    await connectDB(); // 🧠 Wait for MongoDB to connect before starting the server
+    connectCloudinary();
 
-        const app = express();
+    const app = express();
 
-        app.use(cors());
+    app.use(
+      cors({
+        origin: [
+          "http://localhost:5173",
+          "https://bookify-hazel.vercel.app",
+        ],
+        credentials: true,
+      })
+    );
 
-        app.post("/api/stripe", express.raw({ type: "application/json" }), stripeWebHooks)
+    app.post(
+      "/api/stripe",
+      express.raw({ type: "application/json" }),
+      stripeWebHooks
+    );
 
+    app.use(express.json());
+    app.use(clerkMiddleware());
 
-        app.use(express.json());
-        app.use(clerkMiddleware());
+    app.use("/api/clerk", clerkwebhook);
 
+    app.get("/", (req, res) => {
+      res.send("API is working");
+    });
 
+    app.use("/api/user", userRouter);
+    app.use("/api/hotels", hotelRouter);
+    app.use("/api/rooms", roomRouter);
+    app.use("/api/booking", bookingRouterRouter);
 
-        app.use("/api/clerk", clerkwebhook);
-
-        app.get("/", (req, res) => {
-            res.send("API is working");
-        });
-
-        app.use("/api/user", userRouter)
-        app.use("/api/hotels", hotelRouter)
-        app.use("/api/rooms", roomRouter)
-        app.use("/api/booking", bookingRouterRouter)
-
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
-
-            console.log(`Server is running on port http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error("Failed to connect to DB:", error);
-        process.exit(1); // Exit process if DB fails
-    }
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to DB:", error);
+    process.exit(1); // Exit process if DB fails
+  }
 };
 
 startServer();
